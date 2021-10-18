7d50cbf6d821bbfb220ef9fc130471c9d1a26f21
makemile <mkengya@gmail.com>
2021-10-18 17:32:29 -0500

    instalación reactfire react-router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebaseConfig from './utils/firebaseConfig';
+import {FirebaseAppProvider} from 'reactfire';
+import { Suspense } from 'react';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <FirebaseAppProvider firebaseConfig={ firebaseConfig }>
+        <Suspense fallback={<p>Cargando...</p>}>
+            <App/>
+        </Suspense>
+    </FirebaseAppProvider>
+),
   document.getElementById('root')
 );
 
