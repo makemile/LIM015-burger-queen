f3023adf599e2d885cbe0cbcbe02e94fa6a0e150
makemile <mkengya@gmail.com>
2021-10-19 17:35:04 -0500

    se agregó logo en header y colecció de datos fire

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,20 +1,18 @@
 import './header.css';
-import logo from "../images/burger-queen.png"
+import logo from '../assets/burgerqueen1.png';
 
-const Header = () => {
-    return (
-    <header>
-        <img className="logo" src ={logo} alt="logo"/>
-        <nav>
-            <ul>
-            <li><a href="/" className="home">HOME</a></li>
-            <li><a href="/" className="table">MESAS</a></li>
-            <li><a href="/" className="kitchen">COCINA</a></li>
-            <li><a href="/" className="orders">PEDIDOS POR ENTREGAR</a></li>
-            </ul>
-        </nav>
-    </header>
-    );
-}
+const Header = () => (
+  <header>
+    <img className="logo" src= {logo} alt="logo" />
+    <nav>
+      <ul>
+        <li><a href="/" className="home">HOME</a></li>
+        <li><a href="/" className="table">MESAS</a></li>
+        <li><a href="/" className="kitchen">COCINA</a></li>
+        <li><a href="/" className="orders">PEDIDOS POR ENTREGAR</a></li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
