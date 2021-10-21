e351b247003cb3236b4d8c9210a3bdadde83d7c2
Pierina Montalva <pierinamfatur@gmail.com>
2021-10-21 07:00:34 -0500

    templates para menu y se intento agregar funcionalidad a botones de menu

---
+++ my-app/src/components/Lunch.js
@@ -0,0 +1,141 @@
+import React from 'react';
+
+function Lunch() {
+  return (
+    <section className="menu-section-lunch">
+
+      {/* <!------------------------------ Contenedor 1 --------------------------------> */}
+      <div className="container-burger">
+        <div className="content-burger">
+          <div className="visual-burger">
+            <figure className="burger-img">
+              <img src="https://i.postimg.cc/gJLzn9KP/hmb-doble.png" alt="" />
+              <span className="price-burger"><strong>S/. 15.00</strong></span>
+            </figure>
+          </div>
+
+          <div className="product-details-burger">
+            <h3>Hamburguesa Doble</h3>
+          </div>
+
+          <div className="select-opt">
+            <select name="burger-types">
+              <option value="value1">res</option>
+              <option value="value2" selected>vegetariana</option>
+              <option value="value3">pollo</option>
+            </select>
+          </div>
+
+          <div className="additions">
+            <p><strong>Agregados S/.1</strong></p>
+            <input type="radio" id="queso" name="type" value="queso" />
+            <label htmlFor="queso">Queso</label>
+
+            <input type="radio" id="huevo" name="type" value="huevo" />
+            <label htmlFor="huevo">Huevo</label>
+          </div>
+          <button type="button">AGREGAR</button>
+        </div>
+      </div>
+
+      {/* <!------------------------------ Contenedor 2 --------------------------------> */}
+      <div className="container-burger">
+        <div className="content-burger">
+          <div className="visual-burger">
+            <figure className="burger-img">
+              <img src="https://i.postimg.cc/gJLzn9KP/hmb-doble.png" alt="" />
+              <span className="price-burger"><strong>S/. 15.00</strong></span>
+            </figure>
+          </div>
+
+          <div className="product-details-burger">
+            <h3>Hamburguesa Doble</h3>
+          </div>
+
+          <div className="select-opt">
+            <select name="burger-types">
+              <option value="value1">res</option>
+              <option value="value2" selected>vegetariana</option>
+              <option value="value3">pollo</option>
+            </select>
+          </div>
+
+          <div className="additions">
+            <p><strong>Agregados S/.1</strong></p>
+            <input type="radio" id="queso" name="type" value="queso" />
+            <label htmlFor="queso">Queso</label>
+
+            <input type="radio" id="huevo" name="type" value="huevo" />
+            <label htmlFor="huevo">Huevo</label>
+          </div>
+          <button type="button">AGREGAR</button>
+        </div>
+      </div>
+
+      {/* <!------------------------------ Contenedor 3 --------------------------------> */}
+      <div className="container-lunch">
+        <div className="content-lunch">
+          <div className="visual-lunch">
+            <figure className="lunch-figure">
+              <img src="https://i.postimg.cc/XJhnmfyk/burgerking-cafe.png" alt="" />
+              <span className="lunch-price"><strong>S/. 5.00</strong></span>
+            </figure>
+          </div>
+          <div className="lunch-details">
+            <h3>Café Americano</h3>
+          </div>
+          <button type="button" className="lunch-btn">AGREGAR</button>
+        </div>
+      </div>
+      {/* <!------------------------------ Contenedor 4 --------------------------------> */}
+      <div className="container-lunch">
+        <div className="content-lunch">
+          <div className="visual-lunch">
+            <figure className="lunch-figure">
+              <img src="https://i.postimg.cc/XJhnmfyk/burgerking-cafe.png" alt="" />
+              <span className="lunch-price"><strong>S/. 5.00</strong></span>
+            </figure>
+          </div>
+          <div className="lunch-details">
+            <h3>Café Americano</h3>
+          </div>
+          <button type="button" className="lunch-btn">AGREGAR</button>
+        </div>
+      </div>
+      {/* <!------------------------------ Contenedor 5 --------------------------------> */}
+      <div className="container-lunch">
+        <div className="content-lunch">
+          <div className="visual-lunch">
+            <figure className="lunch-figure">
+              <img src="https://i.postimg.cc/XJhnmfyk/burgerking-cafe.png" alt="" />
+              <span className="lunch-price"><strong>S/. 5.00</strong></span>
+            </figure>
+          </div>
+          <div className="lunch-details">
+            <h3>Café Americano</h3>
+          </div>
+          <button type="button" className="lunch-btn">AGREGAR</button>
+        </div>
+      </div>
+
+      {/* <!------------------------------ Contenedor 6 --------------------------------> */}
+      <div className="container-lunch">
+        <div className="content-lunch">
+          <div className="visual-lunch">
+            <figure className="lunch-figure">
+              <img src="https://i.postimg.cc/XJhnmfyk/burgerking-cafe.png" alt="" />
+              <span className="lunch-price"><strong>S/. 5.00</strong></span>
+            </figure>
+          </div>
+          <div className="lunch-details">
+            <h3>Café Americano</h3>
+          </div>
+          <button type="button" className="lunch-btn">AGREGAR</button>
+        </div>
+      </div>
+
+    </section>
+  );
+}
+
+export default Lunch;
--- my-app/src/components/Products.js
+++ my-app/src/components/Products.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import './products.css';
 import Breakfast from './Breakfast';
 import './breakfast.css';
-// import { useState } from 'react'
+import Lunch from './Lunch';
+import './lunch.css';
 // querySnapshot
 
 function Products() {
+  const [visibleLunch, setLunch] = useState(false);
+  const [visibleBreakfast, setBreakfast] = useState(false);
+
   return (
     <>
       <Header />
       <div className="menu-buttons-container">
-        <button type="button" className="btn-breakfast" onClick={() => { alert('Diste click !!'); }}>DESAYUNO</button>
-        <button type="button" className="btn-lunch">ALMUERZO</button>
+        <button type="button" className="btn-breakfast" onClick={() => setBreakfast(true)}>DESAYUNO</button>
+        <button type="button" className="btn-lunch" onClick={() => setLunch(true)}>ALMUERZO</button>
+        {visibleLunch && <Lunch />}
+        {visibleBreakfast && <Breakfast />}
       </div>
 
       <Breakfast />
