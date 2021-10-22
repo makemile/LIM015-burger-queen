deddca91cdc6b60f7eb10a45d80f2dcebeb85445
Pierina Montalva <pierinamfatur@gmail.com>
2021-10-22 12:07:51 -0500

    corrigiendo errores

--- my-app/src/components/Tables.js
+++ my-app/src/components/Tables.js
@@ -1,54 +1,42 @@
-import React from 'react';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+
 import './tables.css';
 import Header from './Header';
-import { db } from '../utils/firebaseConfig';
+import db from '../utils/firebaseConfig';
 
 const Tables = () => {
   const [data, setData] = useState([]);
-  useEffect(()=>{
-    
+  useEffect(() => {
     db.collection('Table')
-    .get()
-    .then(querySnapshot => {
-      
-      const Table = [];
-      querySnapshot.forEach(doc =>{
-        Table.push(doc.data());
-        
+      .get()
+      .then((querySnapshot) => {
+        const Table = [];
+        querySnapshot.forEach((doc) => {
+          Table.push(doc.data());
+        });
+        setData([...Table]);
       });
-      setData([...Table]);
-  
-    })
-
   }, []);
 
   return (
     <>
-    
-     <Header/>
-        {data.length ? (
-          data.map(Table => (
-            
-            <section>
-            <grid  key = {Table.id} className="tables-container">
-       </grid>
-       </section> 
-          ))
-        ) : (
-<p>hola</p>
 
-    )}
-     
-            
-</>        
-      
-    
+      <Header />
+      {data.length ? (
+        data.map((Table) => (
+
+          <section>
+            <grid key={Table.id} className="tables-container" />
+          </section>
+        ))
+      ) : (
+        <p>hola</p>
+
+      )}
+
+    </>
+
   );
-
-}
-
-
-
+};
 
 export default Tables;
