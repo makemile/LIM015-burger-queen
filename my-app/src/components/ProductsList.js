00234df276607829374cbaaf97de65cae9e1e2ba
Pierina Montalva <pierinamfatur@gmail.com>
2021-10-22 17:18:05 -0500

    se coloco data de desayuno

--- my-app/src/components/ProductsList.js
+++ my-app/src/components/ProductsList.js
@@ -1,48 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import Header from './Header';
-// import './productsList.css';
+import './productsList.css';
+import { collection, onSnapshot } from 'firebase/firestore'; // Importar Firestore
+import db from '../utils/firebaseConfig';
 // import Breakfast from './Breakfast';
 // import './breakfast.css';
 // import Lunch from './Lunch';
 // import './lunch.css';
 
-// const [data, setData] = useState([]);
-// console.log(data);
-// console.log(...data);
-
-// useEffect(() => {
-//   onSnapshot(collection(db, 'Table'), (snapshot) => {
-//     setData(snapshot.docs.map((doc) => doc.data()));
-//   });
-// }, []);
-// const breakfast = () => <div>Contenido de desayuno</div>;
-// const lunch = () => <div>Contenido de almuerzo</div>;
-
 function Products() {
-  // const [isVisible, setIsVisible] = useState(true);
+  // Eventos para los botones
   const [isVisibleBf, setVisibleBf] = useState(true); // contenedor de desayuno visible
   const [isVisibleLunch, setVisibleLunch] = useState(false); // contenedor de almuerzo oculto
-
-  // function changeBfValue() {
-  //   setVisibleBf(!isVisibleBf);
-  // }
-
-  // function chageLunchValue() {
-  //   setVisibleLunch(!isVisibleLunch);
-  // }
 
   function changeValue() {
     setVisibleBf(!isVisibleBf);
     setVisibleLunch(!isVisibleLunch);
   }
 
+  // Data de desayuno
+  const [breakfast, setBreakfast] = useState([]);
+  console.log(breakfast);
+
+  useEffect(() => {
+    onSnapshot(collection(db, 'Desayunos'), (snapshot) => {
+      setBreakfast(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
     <>
-      <button type="button" className="btn-breakfast" onClick={changeValue}>DESAYUNO</button>
-      <button type="button" className="btn-lunch" onClick={changeValue}>ALMUERZO</button>
+      {/* <button type="button" className="btn-breakfast" onClick={changeValue}>DESAYUNO</button>
+      <button type="button" className="btn-lunch" onClick={changeValue}>ALMUERZO</button> */}
 
-      <div style={{ display: isVisibleBf ? 'block' : 'none' }}>Contenido de desayuno</div>
-      <div style={{ display: isVisibleLunch ? 'block' : 'none' }}>Contenido de almuerzo</div>
+      {/* <div style={{ display: isVisibleBf ? 'block' : 'none' }}>
+        Contenido de desayuno
+      </div>
+      <div style={{ display: isVisibleLunch ? 'block' : 'none' }}>
+        Contenido de almuerzo
+      </div> */}
+
+      {/* BOTONES */}
+      <section className="menu-section">
+        <div className="menu-buttons-container">
+          <button type="button" className="btn-breakfast" onClick={changeValue}>DESAYUNO</button>
+          <button type="button" className="btn-lunch" onClick={changeValue}>ALMUERZO</button>
+        </div>
+      </section>
+
+      {/* SECCION DE DESAYUNO */}
+      {breakfast.map((product) => (
+        <section className="menu-section-breakfast" style={{ display: isVisibleBf ? 'block' : 'none' }}>
+          <div className="container-breakfast">
+            <div className="content-breakfast">
+              <div className="visual-breakfast">
+                <figure className="breakfast-figure">
+                  <img src={product.img} alt="" />
+                  <span className="breakfast-price">
+                    <strong>{product.price}</strong>
+                  </span>
+                </figure>
+              </div>
+              <div className="breakfast-details">
+                <h3>{product.name}</h3>
+              </div>
+              <button type="button">AGREGAR</button>
+            </div>
+          </div>
+        </section>
+      ))}
+      {/* ALMUERZO */}
+      <div style={{ display: isVisibleLunch ? 'block' : 'none' }}>
+        Contenido de almuerzo
+      </div>
 
     </>
   );
