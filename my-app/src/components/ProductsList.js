d688b2eaad3ddd4a38f2c5a23feb08975a364e82
Pierina Montalva <pierinamfatur@gmail.com>
2021-10-23 08:44:32 -0500

    prueba cambiando texto de boton

--- my-app/src/components/ProductsList.js
+++ my-app/src/components/ProductsList.js
@@ -25,7 +25,8 @@ function Products() {
       {/* BOTONES */}
         <section className="menu-section">
           <div className="menu-buttons-container">
-            <button type="button" className="btn-breakfast" onClick={changeValue}>DESAYUNO</button>
+            <button type="button" className="btn-breakfast" onClick={changeValue}>{ isVisibleBf ? "DESAYUNO" : "ALMUERZO"}
+</button>
             <button type="button" className="btn-lunch" onClick={changeValue}>ALMUERZO</button>
           </div>
         </section>
