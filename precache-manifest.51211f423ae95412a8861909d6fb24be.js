68688b501024ca3fa8e9feed5bb17171bed1663a
Kengya <mkengya@gmail.com>
2021-12-29 13:46:58 -0500

    Updates

---
+++ precache-manifest.51211f423ae95412a8861909d6fb24be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eacad80ce8beed8c79df38b5d48a70c",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "50103a5ef73712eddaf4",
+    "url": "/repository-name/static/css/main.0636914e.chunk.css"
+  },
+  {
+    "revision": "ffbe24961f19a6d6205a",
+    "url": "/repository-name/static/js/2.e5fe269c.chunk.js"
+  },
+  {
+    "revision": "66a43cca78792b0fc8fdddb40bb2971b",
+    "url": "/repository-name/static/js/2.e5fe269c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0a06ddbbcb884a7680",
+    "url": "/repository-name/static/js/3.6f0763fd.chunk.js"
+  },
+  {
+    "revision": "50103a5ef73712eddaf4",
+    "url": "/repository-name/static/js/main.764c159e.chunk.js"
+  },
+  {
+    "revision": "cae806819ca561c16554",
+    "url": "/repository-name/static/js/runtime-main.d99a3b55.js"
+  },
+  {
+    "revision": "c36ae3c8bb26b7332a73fb2cf9dc018d",
+    "url": "/repository-name/static/media/burger-queen.c36ae3c8.png"
+  },
+  {
+    "revision": "c36ae3c8bb26b7332a73fb2cf9dc018d",
+    "url": "/repository-name/static/media/logoHeader.c36ae3c8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.286d4781e6df840b03834febaa652378.js"
+  "/repository-name/precache-manifest.51211f423ae95412a8861909d6fb24be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
