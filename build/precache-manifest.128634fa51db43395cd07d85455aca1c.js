999adc3082ff1ea6f1efd9ae363b5d67dd1d51d7
Kengya <mkengya@gmail.com>
2022-02-10 11:46:08 -0500

    cambios

---
+++ build/precache-manifest.128634fa51db43395cd07d85455aca1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c17492e1d71f74edea5f121f3ba432",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "50103a5ef73712eddaf4",
+    "url": "/my-app/static/css/main.0636914e.chunk.css"
+  },
+  {
+    "revision": "fc2e22258de58c802d1d",
+    "url": "/my-app/static/js/2.a5008ad6.chunk.js"
+  },
+  {
+    "revision": "66a43cca78792b0fc8fdddb40bb2971b",
+    "url": "/my-app/static/js/2.a5008ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0a06ddbbcb884a7680",
+    "url": "/my-app/static/js/3.6f0763fd.chunk.js"
+  },
+  {
+    "revision": "50103a5ef73712eddaf4",
+    "url": "/my-app/static/js/main.764c159e.chunk.js"
+  },
+  {
+    "revision": "6c38698e57952ee8c351",
+    "url": "/my-app/static/js/runtime-main.59386ada.js"
+  },
+  {
+    "revision": "c36ae3c8bb26b7332a73fb2cf9dc018d",
+    "url": "/my-app/static/media/burger-queen.c36ae3c8.png"
+  },
+  {
+    "revision": "c36ae3c8bb26b7332a73fb2cf9dc018d",
+    "url": "/my-app/static/media/logoHeader.c36ae3c8.png"
+  }
+]);
